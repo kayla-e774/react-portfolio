44f10c8bf6bb71f1ab44ffdcf2d82b35cbf93ae9
Kayla E <kengelstad16@gmail.com>
2025-02-04 00:25:10 -0600

    footer added

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <div className="container">
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item">
+                        <a href="https://github.com/kayla-e774" target="_blank" className="nav-link px-2 text-body-secondary">
+                            Github
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://www.linkedin.com/in/kayla-engelstad-142968148/" target="_blank" className="nav-link px-2 text-body-secondary">
+                            LinkedIn
+                        </a>
+                    </li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
